471c7f5b6195c60abc81040eff70ac67d76ab3c0
Bildurre <egoiastiz@gmail.com>
2023-01-08 17:38:46 +0100

    plantilla

---
+++ webapp/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="utf-8">
+  <title>Proyecto ASIR</title>
+  <link rel="stylesheet" href="estilos.css">
+</head>
+<body>
+  <?php
+  require 'header.html';
+  ?>
+  <div id="cuerpo">
+    <?php
+    require 'nav.html';
+    ?>
+    <div id="contenido">
+      <h2>TOP SELLERS</h2>
+      
+    </div>
+  </div>
+  <?php
+  require 'footer.html';
+  ?>
+</body>
+</html>
